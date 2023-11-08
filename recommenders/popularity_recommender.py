9462aef1e6f21caeb62a58d2b2543b5b71e98690
frostbite127 <141996530+frostbite127@users.noreply.github.com>
2023-11-08 23:56:50 +0530

    task: Task: Build a Popularity Based Recommendation System

--- recommenders/popularity_recommender.py
+++ recommenders/popularity_recommender.py
@@ -5,9 +5,11 @@ import pandas as pd
 def recommend_popular_movies():
 
     ######################################################################################################
-
+# merge-11
     movie_ratings = pd.merge(data_movies, data_ratings, on = 'movieId')
+# calculate movie rating-2,3,4
     movie_ratings = movie_ratings.groupby('title')['rating'].mean().sort_values(ascending = False).head(20)
+# 5
     movie_ratings = movie_ratings.to_dict()
     #######################################################################################################
     response = []
@@ -22,5 +24,5 @@ def recommend_popular_movies():
             "average_rating": movie_ratings[movie]
         })
 
-
+# 6
     return {"status": True, "data": {"message": "Here are some of the popular recommendations.", "results" : response}}
